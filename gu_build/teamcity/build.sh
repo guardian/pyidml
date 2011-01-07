a34dfd559e12e3c7aafa29bfc7cea4c4ae6238c1
Michael Brunton-Spall <michael.brunton-spall@guardian.co.uk>
2011-01-07 17:22:15 +0000

    Add build script

---
+++ gu_build/teamcity/build.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+function do_or_die() {
+  $*
+  if [ $? != 0 ]; then
+    echo "Failed"
+    exit 1
+  fi
+}
+
+if [ -z "${BUILD_NUMBER}" ]; then
+BUILD_NUMBER=DEV
+fi
+
+# assumes working directory is teamcity configs dir
+echo "build.number is ${BUILD_NUMBER}"
+pushd ../..
+if [ -d ve ]; then
+    rm -rf ve
+fi
+echo "creating virtualenv"
+do_or_die virtualenv2.6 ve
+
+echo "cleaning"
+do_or_die ve/bin/python2.6 setup.py clean
+
+echo "installing dependencies"
+do_or_die ve/bin/pip install --no-index -r requirements.txt --upgrade
+do_or_die virtualenv2.6 --relocatable ve
+
+echo "Running tests..."
+do_or_die ve/bin/python2.6 ve/bin/nosetests -e handlers
+echo "Tests completed succesfully"
+
+echo "Building distribution zip"
+do_or_die ve/bin/python2.6 setup.py sdist --formats=zip
+
+echo "Uploading distributon to pypi.gudev.gnl"
+scp 
+echo "Cleaning up"
+
+do_or_die rm -rf ve
+popd
